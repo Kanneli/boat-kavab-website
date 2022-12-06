718ada1c4203b499b440486c61d0a3922fa9d126
joodh.naseer <joodh.naseer@mma.gov.mv>
2022-12-06 20:26:12 +0500

    Added zayan's photo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,25 @@
 <script setup>
+import zayanImg from '@/assets/Zayan.png'
 </script>
 
 <template>
   <main>
-    <div class="page">
-      <h1>
-        Kavab coming soon
-      </h1>
+    <div>
+      <div class="hidden md:flex md:flex-row md:justify-between">
+        <div class="flex">
+        </div>
+        <div class="flex items-center h-screen">
+          <h1 class="mr-24">
+            Kavab coming soon!
+          </h1>
+        </div>
+      </div>
+      <div class="md:hidden mt-6">
+        <h1>
+          Kavab coming soon!
+        </h1>
+      </div>
+      <div class="fixed bottom-0 h-screen w-screen bg-no-repeat bg-left-bottom bg-contain" :style="`background-image: url('${zayanImg}')` "></div>
     </div>
   </main>
 </template>
